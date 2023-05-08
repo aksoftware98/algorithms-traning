13dafa95712a118d6ca36e99550f1566f6744e85
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-05-09 00:33:56 +0400

    Implement and test insertion sort

---
+++ src/AlgorithmsAndDataStructure/Sorting.Tests/InsertionSortTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Sorting.Tests
+{
+	public class InsertionSortTests
+	{
+        [Fact]
+        public void InsertionSortIsWorkingAsExpected()
+        {
+            var array = new[] { 2, 10, 8, 7 };
+
+            InsertionSort.Sort(array);
+
+            Assert.True(array.SequenceEqual(new[] { 2, 7, 8, 10 }));
+        }
+    }
+}
+
---
+++ src/AlgorithmsAndDataStructure/Sorting/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Sorting
+{
+
+
+	/// <summary>
+	/// Insertion Sort is very basic
+	/// No extra space needed
+	/// Stable 
+	/// Used for small collections and basic for TimSort and IntraSort (Hybrid)
+	/// </summary>
+	public static class InsertionSort
+	{
+
+
+		public static void Sort<T>(T[] array) where T : IComparable<T>
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				T key = array[i];
+				int j = i - 1; 
+				while (j >= 0 && array[j].CompareTo(key) == 1)
+				{
+					array[j + 1] = array[j];
+					j--;
+				}
+				array[j + 1] = key;  
+			}
+		}
+
+	}
+}
+
