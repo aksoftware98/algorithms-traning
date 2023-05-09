47ad71c74bbaf6d5bb9e03b61b06d9cd9e478cab
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-05-09 16:01:18 +0400

    Solve the question count the inversions in an array

---
+++ src/AlgorithmsAndDataStructure/Sorting.Tests/GeneralSolutionsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Sorting.Tests
+{
+    public class GeneralSolutionsTests
+    {
+        [Theory]
+        [InlineData(new[] { 3, 6, 5, 12, 16, 9}, 3)]
+        public void InversionCountsInArrayShouldWorkAsExpected(int[] array, int expected)
+        {
+            var result = GeneralSolutions.CountInversionsInArray(array);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
---
+++ src/AlgorithmsAndDataStructure/Sorting/GeneralSolutions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Sorting
+{
+	/// <summary>
+	/// Set of problems and their solutions that are realted to sorting
+	/// </summary>
+	public static class GeneralSolutions
+	{
+
+		/// <summary>
+		/// The other solution is to use merge sorting and count everytime we swap to elements
+		/// </summary>
+		/// <param name="array"></param>
+		/// <returns></returns>
+		public static int CountInversionsInArray(int[] array)
+		{
+			int inversionCount = 0;
+			for (int i = 0; i < array.Length; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (array[i] == array[j])
+						continue;
+					if (array[i] < array[j])
+						inversionCount++; 
+				}
+			}
+
+			return inversionCount;
+		}
+
+	}
+}
+
