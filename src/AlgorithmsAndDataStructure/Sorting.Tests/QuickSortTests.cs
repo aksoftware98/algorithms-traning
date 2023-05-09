71181725aa2ddc03d80a7e4e5b2280808da0df61
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-05-10 03:27:53 +0400

    Implement and test the naive lomuto's quick sort partitioning

---
+++ src/AlgorithmsAndDataStructure/Sorting.Tests/QuickSortTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Sorting.Tests
+{
+    public class QuickSortTests
+    {
+        [Theory]
+        [InlineData(new[] { 3, 2, 1, 78, 9798, 97 }, 0, 5, new[] { 3, 2, 1, 78, 97, 9798})]
+        public void NaviePartioningShoudWorkAsExpected(int[] array, int low, int high, int[] expected)
+        {
+            QuickSort.PartionNaive(array, low, high);
+
+            Assert.True(array.SequenceEqual(expected));
+        }
+    }
+}
+
---
+++ src/AlgorithmsAndDataStructure/Sorting/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Sorting
+{
+	public static class QuickSort
+	{
+
+		public static void PartionNaive<T>(T[] array, int low, int height) where T : IComparable<T>
+		{
+			var pivot = array[height];
+			var tempArray = new T[array.Length];
+
+			int firstIndex = 0;
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i].CompareTo(pivot) == -1)
+				{
+					tempArray[firstIndex] = array[i];
+					firstIndex++; 
+				}
+			}
+
+			tempArray[firstIndex] = pivot;
+			firstIndex++;
+			for (int i = 0; i < array.Length; i++)
+			{
+                if (array[i].CompareTo(pivot) == 1)
+                {
+                    tempArray[firstIndex] = array[i];
+                    firstIndex++;
+                }
+            }
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = tempArray[i];
+			}
+		}
+
+	}
+}
+
