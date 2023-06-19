77ac93984685f0f02021e121e9ba52b093946f76
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-06-19 12:21:16 +0400

    Implement and understand Counting Sort

---
+++ src/AlgorithmsAndDataStructure/Sorting.Tests/CountingSortTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.Tests
+{
+	public class CountingSortTests
+	{
+
+		[Theory]
+		[InlineData(new int[] { 1, 4, 4, 1, 0, 1 }, 5, new int[] { 0, 1, 1, 1, 4, 4 })]
+		public void NaiveSort_Returns_Sorted_Array(int[] unsorted, int k,int[] expected)
+		{
+			// Arrange
+			var array = unsorted;
+
+			// Act
+			CountingSort.NaiveSort(array, k);
+
+			// Assert
+			Assert.Equal(expected, array);
+		}
+
+		[Theory]
+		[InlineData(new int[] { 1, 4, 4, 1, 0, 1 }, 5, new int[] { 0, 1, 1, 1, 4, 4 })]
+		public void Sort_Returns_Sorted_Array(int[] unsorted, int k, int[] expected)
+		{
+			// Arrange
+			var array = unsorted;
+
+			// Act
+			CountingSort.Sort(array, k);
+
+			// Assert
+			Assert.Equal(expected, array);
+		}
+
+
+	}
+}
---
+++ src/AlgorithmsAndDataStructure/Sorting/CountingSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+
+	/// <summary>
+	/// Counting Sort is a non-comparison integer sorting algorithm. It sorts an array based on the keys that are small integers.
+	/// It's stable 
+	/// </summary>
+	public class CountingSort
+	{
+		public static void NaiveSort(int[] array, int k)
+		{
+			int[] count = new int[k];
+
+			for (int i = 0; i < k; i++)
+				count[i] = 0;
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				count[array[i]]++;
+			}
+
+			int index = 0; 
+			for (int i = 0; i < k; i++)
+			{
+				for(int j = 0; j < count[i]; j++)
+				{
+					array[index] = i;
+					index++;
+				}
+			}
+		}
+
+		public static void Sort(int[] array, int k)
+		{
+			var count = new int[k];
+			for (int i = 0; i < k; i++)
+			{
+				count[i] = 0; 
+			}
+			for (int i = 0; i < array.Length; i++)
+			{
+				count[array[i]]++;
+			}
+
+			// Get the count of the minimum numbers that are less than or equal to the current number
+			for (int i = 1; i < k; i++)
+			{
+				count[i] = count[i - 1] + count[i];
+			}
+
+			var output = new int[array.Length];
+			for (int i = array.Length - 1; i >= 0; i--)
+			{
+				output[count[array[i]] - 1] = array[i];
+				count[array[i]]--; 
+			}
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = output[i];
+			}
+		}
+	}
+}
