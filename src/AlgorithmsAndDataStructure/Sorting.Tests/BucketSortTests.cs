e832c139d5f23f2164a74adaa0d51df26e109249
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-06-19 23:33:39 +0400

    Implement and understand Bucket Sort

---
+++ src/AlgorithmsAndDataStructure/Sorting.Tests/BucketSortTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.Tests
+{
+	public class BucketSortTests
+	{
+
+		[Theory]
+		[InlineData(new int[] { 20, 88, 70, 85, 75, 95, 18, 82, 60 }, 5, new int[] { 18, 20, 60, 70, 75, 82, 85, 88, 95 })]
+		public void Sort_Returns_Sorted_Array(int[] unsorted, int k, int[] expected)
+		{
+			// Arrange
+			var array = unsorted;
+
+			// Act
+			BucketSort.Sort(array, k);
+
+			// Assert
+			Assert.Equal(expected, array);
+		}
+
+
+	}
+}
---
+++ src/AlgorithmsAndDataStructure/Sorting/BucketSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+	/// <summary>
+	/// Uniform distribution
+	/// Linear time 
+	/// </summary>
+	public class BucketSort
+	{
+
+		public static void Sort(int[] array, int k)
+		{
+			int n = array.Length;
+			int max = array[0];
+			int min = array[0]; 
+			for (int i = 0; i < n; i++)
+			{
+				if (array[i] > max)
+					max = array[i];
+				if (array[i] < min)
+					min = array[i];
+			}
+
+			List<List<int>> buckets = new(); 
+			for (int i = 0; i < k; i++)
+			{
+				buckets.Add(new());
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				var bucket = k * array[i] / max;
+				if (bucket == k)
+					bucket--;
+				buckets[bucket].Add(array[i]);
+			}
+
+			for (int i = 0; i < k; i++)
+			{
+				buckets[i] = buckets[i].Order().ToList(); 
+			}
+
+			int index = 0;
+			for (int i = 0; i < k; i++)
+			{
+				for (int j = 0; j < buckets[i].Count; j++)
+				{
+					array[index] = buckets[i][j];
+					index++; 
+				}
+			}
+
+			
+		}
+
+
+	}
+}
